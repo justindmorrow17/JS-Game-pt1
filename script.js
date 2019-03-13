39d3743cb7fe24c2fef9d5bf6bceabb0e88ec060
Justin <prankjt@gmail.com>
2019-03-12 20:14:48 -0400

    cleaned up some errors and the code

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 let start = prompt("Would you like to play a game?");
-if (start === "yes") {
+if (start.toLowerCase() === "yes") {
     let playerName = prompt("What is your name?");
     let playerhp = 40;
     let granthp = 10;
@@ -9,22 +9,23 @@ if (start === "yes") {
     // console.log(`${playerName}`) Test 
     console.log(`Let's get ready to rumble! ${playerName}'s starting health is ${playerhp} and Grant's starting health is ${granthp}`);
 
-    while (granthp > 0) {
-        console.log(`${playerName}'s Health: ${playerhp -= Math.floor((Math.random()* 12) + 1)}`);
+    while (start) {
+        console.log(`${playerName}'s Health: ${playerhp -= Math.floor((Math.random()* 2) + 1)}`);
         console.log(`Grant's Health: ${granthp -= Math.floor((Math.random()* 2) + 1)}`);
 
-        if (wins === 2 && granthp <= 0) {
-            console.log(`We have a new Champion, ${playerName} wins!`);
-            break;
-
-        } else if (playerhp <= 0) {
+        if (playerhp <= 0) {
             console.log(`You have been found wanting. Game Over - Grant Won!`);
             break;
 
         } else if (granthp <= 0) {
             wins++;
+            console.log(`${playerName} wins! You need to win ${3-wins} more round(s). `);
             granthp = 10;
-            console.log(`${playerName} wins!, you now have ${wins} wins.`);
+
+        } 
+        if (wins === 3) {
+            console.log(`We have a new Champion, you won the game.`);
+            break;
         }
     }
 
@@ -37,7 +38,6 @@ if (start === "yes") {
 // let pHp = 40;
 // let GrantHp = 10;
 
-
 // **Player health decreases**
 // for (let php = 40; php <= 40; php--) {
 //     if (php > 0 ) {
@@ -47,7 +47,6 @@ if (start === "yes") {
 //         break;
 //     }
 // }
-
 
 // // **Grant health decreases**
 // for (let Granthp = 10; Granthp <= 10; Granthp--) {
